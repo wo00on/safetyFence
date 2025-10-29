d8466ca44da7f6c3403ac1b53c0a908949d91f88
woon <woony8959@gmail.com>
2025-10-29 20:37:58 +0900

    지도ui변경

--- styles/MapPageStyles.ts
+++ styles/MapPageStyles.ts
@@ -1,43 +1,43 @@
 // constants/customMapStyle.ts
 export const customMapStyle = [
-  // 1. 기본 랜드 색상: 연한 회색 (배경)
+  // 1. 기본 랜드 색상: 흰색 (배경)
   {
     featureType: 'all',
     elementType: 'geometry',
-    stylers: [{ color: '#f5f5f5' }],
+    stylers: [{ color: '#ffffff' }],
   },
   // 2. 기본 라벨 색상: 어두운 회색
   {
     featureType: 'all',
     elementType: 'labels.text.fill',
-    stylers: [{ color: '#ffffffff' }],
+    stylers: [{ color: '#222222' }],
   },
   {
     featureType: 'all',
     elementType: 'labels.text.stroke',
-    stylers: [{ color: '#f5f5f5' }], // 랜드 색상과 동일하게
+    stylers: [{ color: '#ffffff' }], // 랜드 색상과 동일하게
   },
   {
     featureType: 'administrative.land_parcel',
     elementType: 'labels.text.fill',
-    stylers: [{ color: '#bdbdbd' }],
+    stylers: [{ color: '#757575' }],
   },
   // 3. POI (관심 장소) 색상
   {
     featureType: 'poi',
     elementType: 'geometry',
-    stylers: [{ color: '#eeeeee' }],
+    stylers: [{ color: '#f5f5f5' }],
   },
   {
     featureType: 'poi',
     elementType: 'labels.text.fill',
-    stylers: [{ color: '#ffffffff' }],
+    stylers: [{ color: '#222222' }],
   },
   // 4. 공원(Park) 색상
   {
     featureType: 'poi.park',
     elementType: 'geometry',
-    stylers: [{ color: '#dceddc' }], // 연한 녹색 배경
+    stylers: [{ color: '#e6f4e7' }], // 연한 녹색 배경
   },
   {
     featureType: 'poi.park',
@@ -68,7 +68,7 @@ export const customMapStyle = [
   {
     featureType: 'road.local',
     elementType: 'labels.text.fill',
-    stylers: [{ color: '#ffffffff' }],
+    stylers: [{ color: '#333333' }],
   },
   // 6. 대중교통 라인
   {
@@ -85,11 +85,11 @@ export const customMapStyle = [
   {
     featureType: 'water',
     elementType: 'geometry',
-    stylers: [{ color: '#c4f6e9ff' }], // 연한 민트색 배경
+    stylers: [{ color: '#d4eefc' }], // 연한 파란색 배경
   },
   {
     featureType: 'water',
     elementType: 'labels.text.fill',
-    stylers: [{ color: '#2e7d32' }], // 진한 녹색 텍스트
+    stylers: [{ color: '#3d85c6' }], // 파란색 텍스트
   },
 ];
